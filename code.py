ffaaf23ec1516c7ba25a41eaaf4ab5bd7d4d6091
Fake User <no-reply@fakeadress.com>
2021-06-09 08:52:38 +0000

    Update code.py

--- code.py
+++ code.py
@@ -3,3 +3,5 @@ from datetime import datetime
 print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
 
 print("Modified")
+
+print("Adding extra code before merging with extra-branch")
